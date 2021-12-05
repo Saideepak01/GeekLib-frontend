8e330c1efdba5033d1f5ca23f8e139cc6fa3e90c
Sai Deepak <68103212+Saideepak01@users.noreply.github.com>
2021-12-05 21:14:30 +0530

    finished, minor ui imrpovements need to be made

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//geeklib navbar
 const navBar = document.querySelector(".mainScreen");
 navBar.innerHTML = `
 <figure class="title">
@@ -5,6 +6,7 @@ navBar.innerHTML = `
 </figure>
 `;
 
+//main table to hold the data
 const table = document.querySelector(".data");
 table.innerHTML = `
     <table class="tbl">
@@ -24,11 +26,13 @@ table.innerHTML = `
 
 let API = "https://geeklib-101.herokuapp.com";
 // let API_dev = http://localhost:2001/
+
+//async operation which fetches the data
 async function loadData() {
   try {
     const data = await fetch(`${API}`);
     const res = await data.json();
-    if (res.length === 0) {
+    if (res.length === 0) { //checks the length of fetched data if 0 then displayes no books avaliable else send the data to the books function
       const tableEmpty = document.querySelector("tbody");
       tableEmpty.innerHTML = `
     <div class="tableEmpty">
@@ -39,7 +43,7 @@ async function loadData() {
       const searchHide = document.querySelector(".group");
       searchHide.classList.add("hide");
     } else {
-      books(res);
+      booksData(res);
     }
   } catch (err) {
     console.log("failed");
@@ -59,8 +63,10 @@ tablettl.innerHTML = `
   </div>
 </div>
 `;
+ 
+let flag; //flag to check route which opeartion is to be carried out at book() function if false add operation else update
 
-let flag;
+//onClick of add button the addnewbook fn is called and the modal is activated and "add a new book" word is send as parameter to the modal fn and conditionally renders the modal title so that we can know that we are editing or adding
 function addNewBook() {
   let modal = document.querySelector(".modal");
   modal.classList.add("modal_active");
@@ -68,7 +74,9 @@ function addNewBook() {
   modals(wordTtlAdd);
   flag = false;
 }
-function books(list) {
+
+//after loading the data from fetch operation it is checked and sent to this booksData fn here according to the avaliable data the rows are created and appended
+function booksData(list) {
   const tableBody = document.querySelector("tbody");
   list.map((books, index) => {
     let row = document.createElement("tr");
@@ -77,13 +85,6 @@ function books(list) {
     let author = document.createElement("td");
     let genre = document.createElement("td");
     let edition = document.createElement("td");
-
-    sNo.setAttribute("data-label", "S.No");
-    sNo.setAttribute("class", "test456");
-    name.setAttribute("data-label", "Name");
-    author.setAttribute("data-label", "Author");
-    genre.setAttribute("data-label", "Genre");
-    edition.setAttribute("data-label", "Edition");
 
     name.innerHTML = books.Name;
     author.innerHTML = books.Author;
@@ -98,12 +99,12 @@ function books(list) {
     row.appendChild(edition);
 
     let edit = document.createElement("button");
-    edit.setAttribute("onClick", `editContent()`);
+    edit.setAttribute("onClick", `editContent()`);//edit button
     edit.className = "edit btn_styling";
     edit.innerText = "Edit";
 
     let del = document.createElement("button");
-    del.setAttribute("onClick", `deleteContent()`);
+    del.setAttribute("onClick", `deleteContent()`);//delete button
     del.className = "delete btn_styling";
     del.innerText = "Delete";
 
@@ -114,6 +115,8 @@ function books(list) {
   });
 }
 
+//onClick of edit button the editContent() fn is called and the modal is activated and "Edit book details" word is send as parameter to the modal fn and conditionally render the modal title so that we can know that we are editing or adding 
+//and also the name, author, genre, edition details of the specific row is sent as parameter to the modal to edit the data 
 function editContent() {
   const editId = event.target.parentNode.innerText;
   const [, names, author, genre, edition] = editId.split("\t");
@@ -126,6 +129,7 @@ function editContent() {
   flag = true;
 }
 
+//after clicking edit or add button the modals function is activated it conditionally renders and checks if it has to display add or edit
 function modals(wordChange, name, author, genre, edition) {
   const modal = document.querySelector(".modal");
   modal.innerHTML = `
@@ -134,7 +138,7 @@ function modals(wordChange, name, author, genre, edition) {
           <p>${wordChange}</p>
           <button onClick="close_Btn()" class="close_btn"><img class="styling_close" src="Components/SVG/Close_icon.svg" alt="close_button" /></button>
         </div>
-          <form class="modal1_form">
+          <form class="modal1_form" autocomplete="off">
             <div class="modal1_firstPart">
                   <label for="name">Name</label><br />
                   <input type="text" id="name" placeholder="Enter book name" value=${
@@ -164,12 +168,14 @@ function modals(wordChange, name, author, genre, edition) {
 `;
 }
 
+//onclicking the save button in the modal it calls the book() function here name is accepeted as parameter if edit operation occurs then this name parameter is used to query the DB in the backend
 async function book(name) {
   let newName = document.querySelector("#name").value;
   let newEdition = document.querySelector("#edition").value;
   let newGenre = document.querySelector("#genre").value;
   let newAuthor = document.querySelector("#author").value;
 
+  //validates the form if empty raises an alert else it send the updated data or new data to be added to the DB.
   if (
     newName === "" ||
     newEdition === "" ||
@@ -184,6 +190,8 @@ async function book(name) {
       Genre: newGenre,
       Edition: newEdition,
     };
+
+    //the flag variable we previously used if checked here if true update opreation takes place or else add operation takes place
     if (flag) {
       try {
         const updatedData = await fetch(`${API}/${name}`, {
@@ -221,12 +229,16 @@ async function book(name) {
     }
   }
 }
+
+//after the details filled in the modal and submitted successfully the form input values gets reseted
 function refresh() {
   document.querySelector("#name").value = "";
   document.querySelector("#edition").value = "";
   document.querySelector("#genre").value = "";
   document.querySelector("#author").value = "";
 }
+
+//onclick of delete button, deleteContent() fn is called and a warning modal is displayed wheather to delete the specific row from the DB or not
 async function deleteContent() {
   const editId = event.target.parentNode.innerText;
   let [, bkname] = editId.split("\t");
@@ -248,6 +260,7 @@ async function deleteContent() {
   `;
 }
 
+//if yes button is clicked deleteData() fn is initiated and the row is deleted from the DB
 async function deleteData(condition) {
   try {
     const delData = await fetch(`${API}/${condition}`, {
@@ -262,21 +275,25 @@ async function deleteData(condition) {
   }
 }
 
+//on click of no button delete modal is closed.
 function del_closeBtn() {
   let delModal = document.querySelector(".del_modal");
   delModal.classList.remove("del_modalActive");
 }
 
+//close button for modal
 function close_Btn() {
   let modal = document.querySelector(".modal");
   modal.classList.remove("modal_active");
 }
 
+//load screen
 let spinner = document.querySelector(".loading");
 window.addEventListener("load", function () {
   spinner.style.display = "none";
 });
 
+//search operation for searching using names
 function search() {
   let input, filter, table, tr, td, i, value;
   input = document.querySelector("#txtInp");
